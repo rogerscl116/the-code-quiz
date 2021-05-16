a68d3e8e68d063f0848be7b86d26457bd3fe352d
charity.rogers <rogerscl116@gmail.com>
2021-05-16 07:41:32 -0500

    add html and pseudocode

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+// add array of questions and answers
+var questions = [
+    {
+        q: "What does HTML stand for?",
+        c: ["home tool markup language", "hyperlinks and text manipulation language", "hypertext markup language", "home tool manipulation language"],
+        a: "hypertext markup language"
+    },
+    {
+        q: "Math.random() returns a random number between what two numbers?",
+        c: ["1 and 9", "5 and 15", "0 and 100", "0 and 1"],
+        a: "0 and 1"
+    },
+    {
+        q: "What HTML tag is used to define an internal style sheet?",
+        c: ["<style>", "<script>", "<css>", "none of these"],
+        a: "<style>"
+    },
+    {
+        q: "String values must be enclosed within ____ when being assigned to variables.",
+        c: ["commas", "quotes", "curly brackets", "parenthesis"],
+        a: "quotes"
+    },
+    {
+        q: "A JavaScript Boolean represents one of two possible values. What are the two values?",
+        c: ["true or false", "numbers or letters", "functions or objects", "arrays or dates"],
+        a: "true or false"
+    },
+];
+
+// add declared variables
+
+
+// create function to display the questions
+
+
+// create function to check the answers
+
+
+// create function to append the last page
+
+
+// add event listener when user clicks start quiz
