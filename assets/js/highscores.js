57108c8f8a799d3d941dd8696ff5493f7ad4a3f1
charity.rogers <rogerscl116@gmail.com>
2021-05-16 16:46:43 -0500

    add variables and local storage to highscores

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -1,10 +1,24 @@
 // add declared variables
-
+var highScores = document.querySelector("#highScores");
+var clearScores = document.querySelector("#clearScores");
+var goBack = document.querySelector("#goBack");
 
 // get local storage
+var allScores = localStorage.getItem("allScores");
+allScores = JSON.parse(allScores);
 
+if (allScores !== null) {
+    for (var i = 0; i < allScores.length; i++) {
+        var createLi = document.createElement("li");
+        createLi.textContent = allScores[i].initials + " " + allScores[i].score;
+        highScores.appendChild(createLi);
+    }
+}
 
 // create eventlistener to clear scores
-
+clearScores.addEventListener("click", function() {
+    localStorage.clear();
+    location.reload();
+});
 
 // create eventlistener to go back to index page
